e3da87df80025ecfbfe8fe1628b613cf7ece71b7
Wahidul Azam <wahidulazam@Wahiduls-MacBook-Pro.local>
2024-03-12 17:24:59 +0600

    global exception handler added to produce custome exception, but there is room to do better, customer add request validation added

--- src/main/java/com/restaurant/restaurant/modules/customer/dto/CustomerAddRequest.java
+++ src/main/java/com/restaurant/restaurant/modules/customer/dto/CustomerAddRequest.java
@@ -1,5 +1,6 @@
 package com.restaurant.restaurant.modules.customer.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @NoArgsConstructor
@@ -8,10 +9,13 @@ import lombok.*;
 @Getter
 @Setter
 public class CustomerAddRequest {
+    @NotBlank(message = "Name is mandatory.")
     private String name;
+    @NotBlank(message = "MobileNumber is mandatory.")
     private String mobileNumber;
     private String gender;
     private String dateOfBirth;
     private String password;
+    @NotBlank(message = "CustomerType is mandatory.")
     private String customerType;
 }
